43eddcdd819bcf0c049f056754f8c3ec1f068e61
Michael B. Kulik <mike.b.kulik@gmail.com>
2013-05-03 00:15:23 -0400

    add Sublime Text 2 preferences

--- link_configs.sh
+++ link_configs.sh
@@ -2,3 +2,4 @@ ln -sf $PWD/vimrc $HOME/.vimrc
 ln -sf $PWD/tmux.conf $HOME/.tmux.conf
 ln -sf $PWD/bashrc $HOME/.bashrc
 ln -sf $PWD/bash_profile $HOME/.bash_profile
+ln -sf $PWD/Preferences.sublime-settings $HOME/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/Preferences.sublime-settings
