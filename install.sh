07ee2587239412088a402bbd4c43f501440d0031
Michael B. Kulik <mike.b.kulik@gmail.com>
2013-05-29 12:00:37 -0400

    remove unused files

--- install.sh
+++ install.sh
@@ -2,4 +2,3 @@ ln -sf $PWD/vimrc $HOME/.vimrc
 ln -sf $PWD/tmux.conf $HOME/.tmux.conf
 ln -sf $PWD/bashrc $HOME/.bashrc
 ln -sf $PWD/bash_profile $HOME/.bash_profile
-ln -sf $PWD/Preferences.sublime-settings $HOME/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/Preferences.sublime-settings
